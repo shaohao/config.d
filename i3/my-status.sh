67408395fc42f60de915161c34e54b8e5b1aa986
Shao Hao <shaohao@live.cn>
2013-08-21 21:21:24 +0800

    add deadbeef proc monitor to i3status. make tclsh/wish support readline with socat.

--- i3/my-status.sh
+++ i3/my-status.sh
@@ -3,19 +3,35 @@
 declare -i p_rx=0
 declare -i p_tx=0
 
+ddb_status() {
+	local _obj=$1 val
+
+	local info
+	if [ `pgrep deadbeef` ]; then
+		info=$(deadbeef --nowplaying "%a - %t")
+	else
+		info='DeaDBeef'
+	fi
+
+	val=$(printf '"full_text":"%s"' "$info")
+
+	eval $_obj="'{$val}'"
+}
+
 goa_status() {
-	local _obj=$1
-	local val
+	local _obj=$1 val
+
 	if [ `pgrep -f 'goagent\/.*proxy\.py'` ]; then
 		val='"full_text":"goa: yes","color":"#00FF00"'
 	else
 		val='"full_text":"goa: no","color":"#FF0000"'
 	fi
+
 	eval $_obj="'{$val}'"
 }
 
 net_status() {
-	local _obj=$1
+	local _obj=$1 val
 
 	cal_speed() {
 		echo $1 $2 | awk '{
@@ -30,7 +46,6 @@ net_status() {
 
 	local c_rx c_tx
 	local a_rx a_tx
-	local val
 	read c_rx < /sys/devices/pci0000\:00/0000\:00\:07.0/net/enp0s7/statistics/rx_bytes
 	read c_tx < /sys/devices/pci0000\:00/0000\:00\:07.0/net/enp0s7/statistics/tx_bytes
 	a_rx=$(cal_speed $c_rx $p_rx)
@@ -49,13 +64,18 @@ i3status | while :
 do
 	read line
 
+	# deadbeef
+	ddb_status ddb_obj
+
 	# go-agent
 	goa_status goa_obj
 
 	# net speed
 	net_status net_obj
 
-	obj="$goa_obj,$net_obj"
+	obj="$ddb_obj,$goa_obj,$net_obj"
 
 	echo $line | sed "s/\[{/[$obj,{/"
 done
+
+# ex: ts=4
