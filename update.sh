f236ffd2650aa6c0749e723a29b7e79137f0442a
Shao Hao <shaohao@live.cn>
2013-10-04 20:29:05 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git subtree pull --prefix=vimfiles https://github.com/shaohao/vimfiles.git        master --squash
+git subtree pull --prefix=oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git master --squash
+
