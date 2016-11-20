35f5f1311175f9903632cb14265caf59477632c1
Shao Hao <shaohao@live.cn>
2016-11-20 23:04:06 +0800

    add powerline as a subtree module.

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git subtree pull --prefix=vimfiles https://github.com/shaohao/vimfiles.git        master --squash
+git subtree pull --prefix=vimfiles  https://github.com/shaohao/vimfiles.git        master --squash
 git subtree pull --prefix=oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git master --squash
+git subtree pull --prefix=powerline https://github.com/powerline/powerline.git    master --squash
 
