965fa408f4e59c729696a857fca780cd067d9151
shaohao <shaohao@outlook.com>
2019-11-06 16:50:13 +0800

    add z

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git subtree pull --prefix=vimfiles  https://github.com/shaohao/vimfiles.git        master --squash
+git subtree pull --prefix=vimfiles  https://github.com/shaohao/vimfiles.git       master --squash
 git subtree pull --prefix=oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git master --squash
 git subtree pull --prefix=powerline https://github.com/powerline/powerline.git    master --squash
-
+git subtree pull --prefix=z         https://github.com/rupa/z.git                 master --squash
